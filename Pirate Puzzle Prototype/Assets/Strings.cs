6f52614066660e519c7dfca11081606e9c624a92
KBrutscher <148461314+KBrutscher@users.noreply.github.com>
2024-02-21 16:14:57 +0100

    added script for strings

---
+++ Pirate Puzzle Prototype/Assets/Strings.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Strings : MonoBehaviour
+{
+    string[] resetGame = { "Reset Game", "Spiel zuruecksetzen" };
+    string[] about = { "About", "Ueber das Spiel" };
+    string[] changeLanguage = { "Change Language", "Sprache aendern" };
+}
