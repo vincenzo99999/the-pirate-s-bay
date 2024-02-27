f5d92d7668da8cf6eecc5116619ad8937832c45c
Arthur <41858848+ArturoHaru@users.noreply.github.com>
2024-02-27 10:07:33 +0100

    in testing

--- Pirate Puzzle Prototype/Assets/Navigation.cs
+++ Pirate Puzzle Prototype/Assets/Navigation.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Apple.Accessibility;
 using TMPro;
 using UnityEngine;
 
 public class Navigation : MonoBehaviour
 {
+    /* IMPORTANT: buttons should always be direct children of Screens or navigation 
+     * will not work because of shenaningans (thanks apple)*/
+
 
     [SerializeField] GameObject screenToGo;
     [SerializeField] TextMeshProUGUI hintText;
@@ -12,7 +16,21 @@ public class Navigation : MonoBehaviour
     [SerializeField] string[] hints;
 
     [SerializeField] int hintNumber = 0;
+    
 
+    private void Start()
+    {
+        
+        AccessibilityNode node = GetComponent<AccessibilityNode>();
+        
+
+        GameObject accessibilityParent = new GameObject(gameObject.name + "_AccParent", typeof(RectTransform));
+        accessibilityParent.transform.position = transform.position;
+        accessibilityParent.transform.SetParent(transform.parent);
+        transform.SetParent(accessibilityParent.transform);
+
+            
+    }
 
     public void NavigateTo()
     {
