b757336914e39fd69ba2d70182730078b21ef572
Arthur <41858848+ArturoHaru@users.noreply.github.com>
2024-03-02 10:35:02 +0100

    Tutorial is now completely playable with voiceover

--- Pirate Puzzle Prototype/Assets/Scripts/FadeIn.cs
+++ Pirate Puzzle Prototype/Assets/Scripts/FadeIn.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class FadeIn : MonoBehaviour
 {
     [SerializeField] Animator animator;
-
+    [SerializeField] GameObject tap;
     
     public void FadeInTrigger()
     {
+        animator.gameObject.SetActive(true);
         animator.SetTrigger("Clicked");
+        tap.SetActive(false);
     }
 }
